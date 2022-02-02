7d47d93f330c9fe58619ee54ffba97306488cdb8
RaimundoJSoares <raianimes1000@gmail.com>
2022-02-02 17:33:59 -0400

    Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import incomeIMG from '../../assets/income.svg';
+import outcomeIMG from '../../assets/outcome.svg';
+import totalIMG from '../../assets/total.svg';
+import { Container } from "./styles";
+
+export function Summary() {
+
+  return(
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeIMG} alt='Entradas'></img>
+        </header>
+        <strong>R$1000 reais</strong>
+      </div>
+      <div>
+        <header>
+          <p> Saidas</p>
+          <img src={outcomeIMG} alt='Saidas'></img>
+        </header>
+        <strong>-R$500 reais</strong>
+      </div>
+      <div className='highlight-background'>
+        <header>
+          <p>Total</p>
+          <img src={totalIMG} alt='total'></img>
+        </header>
+        <strong>R$500 reais</strong>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,35 @@
+import styled  from 'styled-components';
+
+export const Container = styled.div `
+display:grid; //porque são 3 itens do mesmo tamanho, um do lado do outro, mas poderia ser flex
+grid-template-columns:repeat(3, 1fr); //3 colunas de tamanhos iguais, então repete 3 vezes o 1fr
+gap:2rem;
+margin-top: -10rem;
+
+div {
+  background: var(--shape);
+  padding: 1.5rem 2rem;
+  border-radius: 0.25rem;
+  color: var(--text-title);
+  
+  header{
+    align-items:center;
+    display:flex;
+    justify-content:space-between;
+  }
+
+  strong {
+    display: block; //o strong vem com padrão margin inline, dai o margin top não funciona, o display block faz com que o strong se comporte como uma div;
+    margin-top:1rem;
+    font-size:2rem;
+    font-weight: 500;
+    line-height: 3rem;
+  }
+
+  &.highlight-background {
+    background: var(--green);
+    color: #fff;
+  }
+}
+
+`;
